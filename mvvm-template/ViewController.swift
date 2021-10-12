ba1933cc62fdb5ccd611898696c3139f06f571a8
Asad <axad.hayat@gmail.com>
2021-10-12 21:30:50 +0500

    Initial Commit

---
+++ mvvm-template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvm-template
+//
+//  Created by Asad Hayat on 12/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
